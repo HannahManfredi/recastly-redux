da7b603173b3d47dcfa39d8a27b6ea70cbc21280
Kimberly Kung <kimberly.kung@gmail.com>
2021-02-09 20:39:23 -0800

    Complete connecting combine reducer to store

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,9 @@ import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
 
+
 // our state needs to have 3 props
 // videoList
 // video
 //query/search term
+
