c85304ddcf287fbf228cb2be33be8754c1bfe413
Kimberly Kung <kimberly.kung@gmail.com>
2021-02-11 19:22:53 -0800

    Completed Bare Minimum Requirments
    (am a little salty about certain tests asking for certain prop names tho...)

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,7 +5,6 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
-  //pass searchYouTube an options obj with a key, query, max of 5 and a cb
   return (dispatch) => {
     let optionsObj = {
       key: YOUTUBE_API_KEY,
@@ -14,8 +13,7 @@ var handleVideoSearch = (q) => {
     };
 
     var cb = function(data) {
-      //call dispatch from inside of cb???
-      console.log(data);
+
       dispatch(changeVideoList(data));
       dispatch(changeVideo(data[0]));
     };
--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,19 +1,14 @@
 import { connect } from 'react-redux';
 import Search from './../components/Search.js';
-import handleSearchChange from '../actions/search.js';
-
-
-//TODO: define a SearchContainer component which will hook up your action
-// dispatchers with your search component props.
-//HINT: use react-redux 'connect' method to generate a container component from
-//state and dispatch mappings.
+import handleVideoSearch from '../actions/search.js';
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleSearchInputChange: (q) => dispatch(handleSearchChange(q))
+    getYouTubeVideos: (q) => dispatch(handleVideoSearch(q))
   };
 };
 
 var SearchContainer = connect(null, mapDispatchToProps)(Search);
 
 export default SearchContainer;
+
